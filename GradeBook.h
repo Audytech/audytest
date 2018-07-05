963b3881100c6ca7206fc54c4b7e4875f9346efa
Audy Sengdara <audytech@csintellect.com>
2018-07-05 17:27:06 +0800

    c++ testing

---
+++ GradeBook.h
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+// GradeBook class defitions
+
+class GradeBook
+{
+public:
+    GradeBook(string);
+    void setCourseName(string);
+    string getCourseName();
+    void displayMessage();
+    void determineClassAverage();
+
+private:
+    string courseName;
+}; // end class GradeBook
---
+++ student/main.cpp
@@ -0,0 +1,10 @@
+
+#include "../GradeBook.h"
+
+int main(){
+
+  GradeBook myGradeBook ("C++ promgramming");
+
+  myGradeBook.displayMessage(); //display welcome displayMessage
+  myGradeBook.determineClassAverage();
+}// end main function

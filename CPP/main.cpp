f60189f53a358a4273b4411b0c2b8b80aebeee32
Audy Sengdara <audytech@csintellect.com>
2018-07-07 16:50:38 +0700

    try push command

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 int main(){
 
   cout<<"test";
+  cout<<"checking git" << "\n";
+
+  int count=100;
+  int counter;
+  int total;
+
+  for(int i=0;i<=count;i++){
+
+    counter += 1 ;
+  }
+  cout << "this is git testing\n counter total is: " << counter;
   getchar();
 
 return 0;
